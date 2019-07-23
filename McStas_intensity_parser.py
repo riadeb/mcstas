19df60bfd9817f94acdd897d73a7f896d3562b19
riadeb <51858603+riadeb@users.noreply.github.com>
2019-07-23 15:51:47 +0300

    Update and rename main.py to McStas_intensity_parser.py

--- McStas_intensity_parser.py
+++ McStas_intensity_parser.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
-path = r"C:\Users\Riade\Documents\instruments\benchmarkneutrons_0.01_to_3_100values_inc"
+path = r"\instruments\benchmarkneutrons_0.01_to_3_100values_inc" #To change with path of foler that contains the different folders for each value of the parameter
 
 i = 0
 depths, intensities = [], []
 while True:
     try:
-        f = open(path + "\\" + str(i) + "\\mon.dat", "r")
+        f = open(path + "\\" + str(i) + "\\mon.dat", "r") 
         lines = f.readlines()
         depth_par = float(lines[9].split("=")[-1][:-1])
         intensity_par = float(lines[19].split("; Mean")[-0].split("Max=")[-1])
@@ -17,7 +17,7 @@ while True:
     except OSError:
         break
 plt.xlabel("Sample depth")
-plt.ylabel("Intensity")
+plt.ylabel("log(Intensity)")
 log_intensities = np.log(intensities)
 #plt.plot(depths, intensities)
 plt.plot(depths, log_intensities)
